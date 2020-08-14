b370ee25df4460ad1936de2299cabc7a7550f0cd
Hsieh Chin Fan <typebrook@gmail.com>
2020-08-14 17:14:24 +0800

    Sync logic with contours from Maptiler

--- process.lua
+++ process.lua
@@ -18,6 +18,20 @@ end
 
 function way_function(way)
 	local ele = way:Find("ele")
-    way:Layer("contour", false)
-    way:AttributeNumeric("ele", ele)
+    local interval = nil
+    if     ele % 500 == 0 then way:Layer("contour",     false); interval = 500
+    elseif ele % 200 == 0 then way:Layer("contour_z10", false); interval = 200
+    elseif ele % 100 == 0 then way:Layer("contour_z11", false); interval = 100
+    elseif ele %  50 == 0 then way:Layer("contour_z12", false); interval =  50
+    elseif ele %  20 == 0 then way:Layer("contour_z13", false); interval =  20
+    else                       way:Layer("contour_z14", false); interval =  10
+    end
+    way:AttributeNumeric("height", ele)
+
+    local index = ele / interval
+    if     index % 10 == 0 then way:AttributeNumeric("nth_line", 10)
+    elseif index %  5 == 0 then way:AttributeNumeric("nth_line",  5)
+    elseif index %  2 == 0 then way:AttributeNumeric("nth_line",  2)
+    else                        way:AttributeNumeric("nth_line",  1)
+    end
 end
